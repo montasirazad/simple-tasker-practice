1dcde6523b6c78fd2f79244508ff39b2ea89c2cf
Montasir Azad <montasirazad@gmail.com>
2026-08-29 04:42:49 +0600

    new practice simple to do app 5

---
+++ simple-to-do-4/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
+import HeroSection from "./HeroSection";
+import TaskBoard from "./TaskBoard";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="flex flex-col justify-center items-center">
+        <HeroSection />
+        <TaskBoard />
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ simple-to-do-5/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import HeroSection from "./components/HeroSection";
+import NavBar from "./components/NavBar";
+import TaskBoard from "./components/Task/TaskBoard";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <div className="flex flex-col items-center justify-center ">
+        <HeroSection />
+        <TaskBoard />
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
