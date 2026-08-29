9dd82c86286210b1c0c08ad304325446976420a1
Montasir Azad <montasirazad@gmail.com>
2026-08-30 05:04:51 +0600

    add task feature added

--- simple-to-do-7/src/components/Task/AddTaskModal.jsx
+++ simple-to-do-7/src/components/Task/AddTaskModal.jsx
@@ -1,72 +1,103 @@
+import { useState } from "react";
 
-const AddTaskModal = () => {
+const AddTaskModal = ({ onAddTask }) => {
+  const [task, setTask] = useState({
+    id: crypto.randomUUID(),
+    title: "",
+    description: "",
+    tags: [],
+    priority: "",
+    isFavorite: false,
+  });
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    let value = e.target.value;
+    if (name === "tags") {
+      value = value.split(",");
+    }
+    setTask({ ...task, [name]: value });
+  };
+
   return (
-    <form class="mx-auto my-10 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11">
-      <h2 class="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
-        Add New Task
-      </h2>
+    <>
+      <div className="absolute top-0 h-full w-full bg-black/40"></div>
+      <form className="absolute top-1/2 left-1/4 z-11 mx-auto my-10 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11">
+        <h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
+          Add New Task
+        </h2>
 
-      <div class="space-y-9 text-white lg:space-y-10">
-        <div class="space-y-2 lg:space-y-3">
-          <label for="title">Title</label>
-          <input
-            class="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
-            type="text"
-            name="title"
-            id="title"
-            required
-          />
-        </div>
-
-        <div class="space-y-2 lg:space-y-3">
-          <label for="description">Description</label>
-          <textarea
-            class="block min-h-[120px] w-full rounded-md bg-[#2D323F] px-3 py-2.5 lg:min-h-[180px]"
-            type="text"
-            name="description"
-            id="description"
-            required
-          ></textarea>
-        </div>
-
-        <div class="grid-cols-2 gap-x-4 max-md:space-y-9 md:grid lg:gap-x-10 xl:gap-x-20">
-          <div class="space-y-2 lg:space-y-3">
-            <label for="tags">Tags</label>
+        <div className="space-y-9 text-white lg:space-y-10">
+          <div className="space-y-2 lg:space-y-3">
+            <label htmlFor="title">Title</label>
             <input
-              class="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
+              className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
               type="text"
-              name="tags"
-              id="tags"
+              name="title"
+              id="title"
               required
+              value={task.title}
+              onChange={handleChange}
             />
           </div>
 
-          <div class="space-y-2 lg:space-y-3">
-            <label for="priority">Priority</label>
-            <select
-              class="block w-full cursor-pointer rounded-md bg-[#2D323F] px-3 py-2.5"
-              name="priority"
-              id="priority"
+          <div className="space-y-2 lg:space-y-3">
+            <label htmlFor="description">Description</label>
+            <textarea
+              className="block min-h-[120px] w-full rounded-md bg-[#2D323F] px-3 py-2.5 lg:min-h-[180px]"
+              type="text"
+              name="description"
+              id="description"
+              onChange={handleChange}
+              value={task.description}
               required
-            >
-              <option value="">Select Priority</option>
-              <option value="low">Low</option>
-              <option value="medium">Medium</option>
-              <option value="high">High</option>
-            </select>
+            ></textarea>
+          </div>
+
+          <div className="grid-cols-2 gap-x-4 max-md:space-y-9 md:grid lg:gap-x-10 xl:gap-x-20">
+            <div className="space-y-2 lg:space-y-3">
+              <label htmlFor="tags">Tags</label>
+              <input
+                className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
+                type="text"
+                name="tags"
+                id="tags"
+                value={task.tags}
+                required
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="space-y-2 lg:space-y-3">
+              <label htmlFor="priority">Priority</label>
+              <select
+                className="block w-full cursor-pointer rounded-md bg-[#2D323F] px-3 py-2.5"
+                name="priority"
+                id="priority"
+                required
+                value={task.priority}
+                onChange={handleChange}
+              >
+                <option value="">Select Priority</option>
+                <option value="low">Low</option>
+                <option value="medium">Medium</option>
+                <option value="high">High</option>
+              </select>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="mt-16 flex justify-center lg:mt-20">
-        <button
-          type="submit"
-          class="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
-        >
-          Create new Task
-        </button>
-      </div>
-    </form>
+        <div className="mt-16 flex justify-center lg:mt-20">
+          <button
+            onClick={() => onAddTask(task)}
+            type="button"
+            className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
+          >
+            Save
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
 
